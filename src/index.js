fa53dc7261acd70135052968062b930d8c77a9dd
LyudmilaDr <ludralo44@gmail.com>
2023-02-18 10:59:21 +0200

    fix file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import './css/styles.css';
-import {fetchCountries} from './fetchCountries';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
+import {fetchCountries} from './js/fetchCountries';
 
 
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const countryList = document.querySelector('[country-list]');
-const countryInfo = document.querySelector('[country-info]');
+// const countryInfo = document.querySelector('[country-info]');
 
-input.addEventListener('input', debounce(searchСountry,DEBOUNCE_DELAY));
+input.addEventListener('input', debounce(searchСountry(), DEBOUNCE_DELAY));
 
 function searchСountry(e){
   e.preventDefault();
   const inputValue = input.value.trim();
   if(inputValue !== ''){
-    fetchCountries.then(data => {
+    fetchCountries
+    .then(data => {
     if(data.length > 10){
       Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
     }else if(data.length < 10 && data.length >= 2){
@@ -35,9 +36,8 @@ function searchСountry(e){
 
 function renderCountry(arr) {
     const markup = arr
-      .map(({ capital, population, languages, name, flags }) => {
-        return 
-       `<li><img src="${flags.svg}" alt=${name.official} width="25" height="25">
+      .map(({ name,capital,population,flags,languages }) => {
+        return `<li><img src="${flags.svg}" alt=${name.official} width="25" height="25">
         <h2> ${name.official}</h2>
         <p><b>Capital:</b>${capital}</p>
         <p><b>Population:</b>${population}</p>
@@ -49,8 +49,7 @@ function renderCountry(arr) {
   function renderCountriesUl(arr){
     const markup = arr
     .map(({ name, flags }) => {
-        return 
-        `<li><img src="${flags.svg}" alt=${name.official} width="25" height="25">
+        return  `<li><img src="${flags.svg}" alt=${name.official} width="25" height="25">
         <h2> ${name.official}</h2></li>`
         
       })
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,5 @@
 const  fetchCountries = name => {
-    return
-     fetch(`https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`)
+    return fetch('https:restcountries.com/v3.1/name/Ukraine/${name}?fields=name,capital,population,flag,languages')
       .then(response => {
         if (!response.ok) {
           if (response.status === 404) {
